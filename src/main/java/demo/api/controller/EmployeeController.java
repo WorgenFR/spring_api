be53ce4686f2a1c76fbfb58e88ab6e454e8982bd
Antoine Grappin <antoine.grappin@etudiant.univ-reims.fr>
2024-03-17 23:30:32 +0100

    first commit

---
+++ src/main/java/demo/api/controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package demo.api.controller;
+
+import demo.api.model.Employee;
+import demo.api.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * Read - Get all employees
+     * @return - An Iterable object of Employee full filled
+     */
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    /**
+     * Read - Get one employee
+     * @param id The id of the employee
+     * @return An Employee object full filled
+     */
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable("id") final Long id) {
+        Optional<Employee> employee = employeeService.getEmployee(id);
+        if(employee.isPresent()) {
+            return employee.get();
+        } else {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/demo/api/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package demo.api.repository;
+
+import demo.api.model.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
